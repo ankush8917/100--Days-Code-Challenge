48d58e0eb30aef277f022ae87a90422b7ae59bbe
ankush8917 <ankushp0514@gmail.com>
2026-03-06 19:41:18 +0530

    Initial commit : Binary to Decimal

---
+++ src/BinaryToDec.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BinaryToDec {
+    public static int binToDec(int a) {
+        int pow=0;
+        int dec=0;
+        while(a>0){
+            int ld=a%10;
+            a=a/10;
+            dec= dec+ (int)(ld*Math.pow(2,pow));
+            pow++;
+        }
+        return dec;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(BinaryToDec.binToDec(n));
+    }
+}
